d0d043b3baa86327f5531ae8ac84f10207c052fc
leobewater <mmbee888@gmail.com>
2024-03-30 15:24:38 -0400

    logout with js enabled

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import 'modern-normalize/modern-normalize.css';
 	import '../styles/main.scss';
 	import type { LayoutData } from './$types';
+	import { invalidateAll } from '$app/navigation';
 
 	export let data: LayoutData;
 
@@ -10,8 +11,24 @@
 
 {#if user}
 	<p>Hello {user.display_name}</p>
-	<!-- form will work without JS enabled -->
-	<form method="POST" action="/api/auth/logout">
+	<!-- form will use action when JS is disabled otherwise use submit() and invalidate page without full refresh -->
+	<form
+		method="POST"
+		action="/api/auth/logout"
+		on:submit|preventDefault={async () => {
+			const response = await fetch('/api/auth/logout', {
+				method: 'POST',
+				headers: {
+					accept: 'application/json'
+				}
+			});
+
+			// need to invalidate the page without full refresh
+			if (response.ok) {
+				invalidateAll();
+			}
+		}}
+	>
 		<button type="submit">Logout</button>
 	</form>
 {/if}
--- src/routes/api/auth/logout/+server.ts
+++ src/routes/api/auth/logout/+server.ts
@@ -1,9 +1,15 @@
-import { redirect } from '@sveltejs/kit';
+import { json, redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const POST: RequestHandler = ({ cookies }) => {
+export const POST: RequestHandler = ({ cookies, request }) => {
 	cookies.delete('access_token', { path: '/' });
 	cookies.delete('refresh_token', { path: '/' });
 
+	// if headers has accept which is calling from JS
+	if (request.headers.get('accept') === 'application/json') {
+		return json({ success: true });
+	}
+
+  // otherwise redirect 
 	throw redirect(303, '/login');
 };
