5be2f446753c15956b62f0f5cbab476d42464791
leobewater <mmbee888@gmail.com>
2024-03-30 15:28:05 -0400

    move layout button to own component

---
+++ src/lib/components/LogoutButton.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { invalidateAll } from '$app/navigation';
+</script>
+<!-- form will use action when JS is disabled otherwise use submit() and invalidate page without full refresh -->
+<form
+	method="POST"
+	action="/api/auth/logout"
+	on:submit|preventDefault={async () => {
+		const response = await fetch('/api/auth/logout', {
+			method: 'POST',
+			headers: {
+				accept: 'application/json'
+			}
+		});
+
+		// need to invalidate the page without full refresh
+		if (response.ok) {
+			invalidateAll();
+		}
+	}}
+>
+	<button type="submit">Logout</button>
+</form>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import 'modern-normalize/modern-normalize.css';
 	import '../styles/main.scss';
 	import type { LayoutData } from './$types';
-	import { invalidateAll } from '$app/navigation';
+	import { LogoutButton } from '$components';
 
 	export let data: LayoutData;
 
@@ -11,26 +11,7 @@
 
 {#if user}
 	<p>Hello {user.display_name}</p>
-	<!-- form will use action when JS is disabled otherwise use submit() and invalidate page without full refresh -->
-	<form
-		method="POST"
-		action="/api/auth/logout"
-		on:submit|preventDefault={async () => {
-			const response = await fetch('/api/auth/logout', {
-				method: 'POST',
-				headers: {
-					accept: 'application/json'
-				}
-			});
-
-			// need to invalidate the page without full refresh
-			if (response.ok) {
-				invalidateAll();
-			}
-		}}
-	>
-		<button type="submit">Logout</button>
-	</form>
+	<LogoutButton />
 {/if}
 
 <slot />
