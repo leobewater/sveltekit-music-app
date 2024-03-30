ef5b82874d741631273064127b9dbd48fc2c73bf
leobewater <mmbee888@gmail.com>
2024-03-30 15:50:19 -0400

    add button

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,9 @@
 <script>
-  import { Skull } from 'lucide-svelte';
+  import { Button } from '$components';
+import { Skull } from 'lucide-svelte';
 </script>
 <Skull />
 
 <a href="/api/auth/login">Login</a>
+<Button element="a" variant="outline" href="some">Login</Button>
+<Button element="button">Login</Button>
