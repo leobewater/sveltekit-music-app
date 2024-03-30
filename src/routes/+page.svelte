59644e04c24c86ac6899486f195e068c3c358bac
leobewater <mmbee888@gmail.com>
2024-03-30 13:23:12 -0400

    add media breakpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,16 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style lang="scss">
-
+	@use '@unsass/breakpoint';
 	h1 {
 		color: red;
 		font-size: functions.toRem(36);
+    
+		@include breakpoint.up('md') {
+			color: green;
+		}
+		@include breakpoint.up('lg') {
+			color: blue;
+		}
 	}
 </style>
