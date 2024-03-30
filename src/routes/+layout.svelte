a320cb068abad7cc1e1c55f85829453b8268b8b7
leobewater <mmbee888@gmail.com>
2024-03-30 15:18:46 -0400

    add logout api and add logout button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import 'modern-normalize/modern-normalize.css';
-  import '../styles/main.scss'
+	import '../styles/main.scss';
 	import type { LayoutData } from './$types';
 
-  export let data: LayoutData;
+	export let data: LayoutData;
+
+	$: user = data.user;
 </script>
 
-{data.user?.display_name}
+{#if user}
+	<p>Hello {user.display_name}</p>
+	<!-- form will work without JS enabled -->
+	<form method="POST" action="/api/auth/logout">
+		<button type="submit">Logout</button>
+	</form>
+{/if}
+
 <slot />
