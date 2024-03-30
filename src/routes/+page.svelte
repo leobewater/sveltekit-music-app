18fabeeaa870b2733e43282949c1ab9c2734d01e
leobewater <mmbee888@gmail.com>
2024-03-30 13:25:21 -0400

    preprocess breakpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,10 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style lang="scss">
-	@use '@unsass/breakpoint';
 	h1 {
 		color: red;
 		font-size: functions.toRem(36);
-    
+
 		@include breakpoint.up('md') {
 			color: green;
 		}
--- svelte.config.js
+++ svelte.config.js
@@ -5,15 +5,15 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  //preprocess: vitePreprocess(),
-  
-  // import our own sass function for the entire app
-  preprocess: preprocess({
-    scss: {
-      prependData: '@use "../styles/functions";'
-    }
-  }),
+	// for more information about preprocessors
+	//preprocess: vitePreprocess(),
+
+	// import our own sass function for the entire app
+	preprocess: preprocess({
+		scss: {
+			prependData: '@use "../styles/functions";@use "@unsass/breakpoint";'
+		}
+	}),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
