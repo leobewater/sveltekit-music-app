323acc76181323ed781bb73975d297c39f713edb
leobewater <mmbee888@gmail.com>
2024-03-30 13:28:27 -0400

    add main scss styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<input />
+<select>
+  <option>hello</option>
+</select>
+<button>Button</button>
+
 <style lang="scss">
 	h1 {
 		color: red;
