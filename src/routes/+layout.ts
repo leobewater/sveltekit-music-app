cab2b277b1c90f8279a0940ca6b8313fb36be1bb
leobewater <mmbee888@gmail.com>
2024-03-30 14:57:49 -0400

    use root layout to redirect user to login or homepage

---
+++ src/routes/+layout.ts
@@ -0,0 +1,21 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = ({ data, url }) => {
+	// extra user profile , user can be undefined
+	const { user } = data || {};
+
+	// if there is a user, but on the login page, redirect to the homepage
+	if (user && url.pathname === '/login') {
+		throw redirect(307, '/');
+	}
+
+	// if no user, redirect to the login page
+	if (!user && url.pathname !== '/login') {
+		throw redirect(307, '/login');
+	}
+
+	return {
+		user
+	};
+};
