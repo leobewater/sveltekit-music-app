9a7463cfa7f22df5f09e97de7db3a56c8143b20c
leobewater <mmbee888@gmail.com>
2024-03-30 22:43:27 -0400

    add Header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Navigation } from '$components';
+
+</script>
+
+<div class="content">
+	<div class="left">
+    <Navigation desktop={false} />
+  </div>
+	<div class="right">RIght</div>
+</div>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+	}
+</style>
