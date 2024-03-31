cf23261ee68abe21fcbfba39aead66304331c6f3
leobewater <mmbee888@gmail.com>
2024-03-31 17:32:35 -0400

    add no-js support for player

--- src/lib/components/Player.svelte
+++ src/lib/components/Player.svelte
@@ -68,6 +68,10 @@
 	.player {
 		audio {
 			display: none;
+			:global(html.no-js) & {
+				display: block;
+				width: 100%;
+			}
 		}
 		button {
 			width: 12px;
@@ -80,6 +84,9 @@
 				fill: var(--text-color);
 				width: 12px;
 				height: 12px;
+			}
+			:global(html.no-js) & {
+				display: none;
 			}
 		}
 	}
