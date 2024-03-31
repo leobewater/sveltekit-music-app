c0e011d5a48896ac964b98069b521493bcb8d3b3
leobewater <mmbee888@gmail.com>
2024-03-31 09:50:05 -0400

    accept search params in spotify endpoint

--- src/routes/api/spotify/[...path]/+server.ts
+++ src/routes/api/spotify/[...path]/+server.ts
@@ -2,10 +2,10 @@ import type { RequestHandler } from './$types';
 import { SPOTIFY_BASE_URL } from '$env/static/private';
 import { error, json } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ fetch, cookies, params }) => {
+export const GET: RequestHandler = async ({ fetch, cookies, params, url }) => {
 	const accessToken = cookies.get('access_token');
 
-	const response = await fetch(`${SPOTIFY_BASE_URL}/${params.path}`, {
+	const response = await fetch(`${SPOTIFY_BASE_URL}/${params.path}${url.search}`, {
 		headers: {
 			Authorization: `Bearer ${accessToken}`
 		}
