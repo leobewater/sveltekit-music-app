c68a07b21d0f8fb0c9830fbc147588810896c4a6
leobewater <mmbee888@gmail.com>
2024-03-31 06:38:19 -0400

    finish side nav and close before navigate away

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	import { Home, Search, ListMusic, type Icon } from 'lucide-svelte';
-	import { type ComponentType } from 'svelte';
+	import { tick, type ComponentType } from 'svelte';
 	import logo from '$assets/Spotify_Logo_RGB_White.png';
 	import { page } from '$app/stores';
 	import { fade } from 'svelte/transition';
+	import { beforeNavigate } from '$app/navigation';
 
 	export let desktop: boolean;
 
 	// state to keep track navigation is for mobile or desktop
 	let isMobileMenuOpen = false;
 	$: isOpen = desktop || isMobileMenuOpen;
+
+	let openMenuButton: HTMLButtonElement;
+	let closeMenuButton: HTMLButtonElement;
 
 	// icon using component icon
 	const menuItems: { path: string; label: string; icon: ComponentType<Icon> }[] = [
@@ -18,12 +22,21 @@
 		{ path: '/playlists', label: 'Playlists', icon: ListMusic }
 	];
 
-	const openMenu = () => {
+	const openMenu = async () => {
 		isMobileMenuOpen = true;
+		await tick();
+		closeMenuButton.focus();
 	};
-	const closeMenu = () => {
+
+	const closeMenu = async () => {
 		isMobileMenuOpen = false;
+		await tick();
+		openMenuButton.focus();
 	};
+
+	beforeNavigate(() => {
+		isMobileMenuOpen = false;
+	});
 </script>
 
 <!-- stop body from scolling when mobile menu is opened -->
@@ -43,11 +56,15 @@
 	{/if}
 	<nav aria-label="Main">
 		{#if !desktop}
-			<button on:click={openMenu}>Open</button>
+			<button bind:this={openMenuButton} on:click={openMenu} aria-expanded={isOpen}>Open</button>
 		{/if}
-		<div class="nav-content-inner" class:is-hidden={!isOpen}>
+		<div
+			class="nav-content-inner"
+			class:is-hidden={!isOpen}
+			style:visibility={isOpen ? 'visible' : 'hidden'}
+		>
 			{#if !desktop}
-				<button on:click={closeMenu}>Close</button>
+				<button bind:this={closeMenuButton} on:click={closeMenu}>Close</button>
 			{/if}
 			<img src={logo} class="logo" alt="Spotify" />
 			<ul>
@@ -83,7 +100,7 @@
 			opacity: 0.75;
 			z-index: 100;
 			@include breakpoint.up('md') {
-				display: hidden;
+				display: none;
 			}
 		}
 
@@ -149,6 +166,10 @@
 				transform 200ms,
 				opacity 200ms;
 			&.is-hidden {
+				transition:
+					transform 200ms,
+					opacity 200ms,
+					visibility 200ms;
 				transform: translateX(-100%);
 				opacity: 0;
 			}
