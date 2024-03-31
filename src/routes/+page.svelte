7a4406aca9e37d8e8d73af4462680894a380de8a
leobewater <mmbee888@gmail.com>
2024-03-30 22:15:21 -0400

    add homepage content placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <input />
+<div style="height: 1000px">
+  content
+</div>
 <select>
   <option>hello</option>
 </select>
