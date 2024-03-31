c3290cfd7ee49b0a37f544d972bfebbe9a7f3147
leobewater <mmbee888@gmail.com>
2024-03-31 09:55:36 -0400

    fetch 3 playlists using promise.all

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,22 @@
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
-  const res = await fetch('/api/spotify/browse/new-releases?limit=6');
-  const res2 = await fetch('/api/spotify/browse/featured-playlists?limit=6');
-  console.log(await res.json());
-  console.log(await res2.json());
+export const load: PageLoad = async ({ fetch, parent }) => {
+	const { user } = await parent();
+
+	// let fetch them in parallel without awaiting
+	const newReleases = fetch('/api/spotify/browse/new-releases?limit=6');
+	const featuredPlaylists = fetch('/api/spotify/browse/featured-playlists?limit=6');
+	const userPlaylists = fetch(`/api/spotify/users/${user?.id}/playlists?limit=6`);
+
+	const [newReleasesRes, featuredPlaylistsRes, userPlaylistsRes] = await Promise.all([
+		newReleases,
+		featuredPlaylists,
+		userPlaylists
+	]);
+
+	return {
+		newReleasesRes: newReleasesRes.ok ? newReleasesRes.json() : undefined,
+		featuredPlaylistsRes: featuredPlaylistsRes.ok ? featuredPlaylistsRes.json() : undefined,
+		userPlaylistsRes: userPlaylistsRes.ok ? userPlaylistsRes.json() : undefined
+	};
 };
