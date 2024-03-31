93f4f48838a78cb2e0357c9689568e6524e30730
leobewater <mmbee888@gmail.com>
2024-03-31 07:13:20 -0400

    make icon button as button

--- src/lib/components/IconButton.svelte
+++ src/lib/components/IconButton.svelte
@@ -10,11 +10,18 @@
 
 	export let icon: ComponentType<Icon>;
 	export let label: string;
+
+	let button: HTMLButtonElement;
+
+  // expose this to navigation
+	export function getButton() {
+		return button;
+	}
 </script>
 
-<button on:click on:mouseover on:focus on:keydown {...$$restProps}>
+<button bind:this={button} on:click on:mouseover on:focus on:keydown {...$$restProps}>
 	<svelte:component this={icon} color="var(--text-color)" aria-hidden="true" focusable="false" />
-  <span class="visually-hidden">{label}</span>
+	<span class="visually-hidden">{label}</span>
 </button>
 
 <style lang="scss">
