2b8aef5f86942520ff317aa56af68f7f9f7b1a96
leobewater <mmbee888@gmail.com>
2024-04-01 09:08:22 -0400

    add toast message type

--- src/lib/stores/toasts.ts
+++ src/lib/stores/toasts.ts
@@ -1,9 +1,15 @@
 import { writable } from 'svelte/store';
 import uniqid from 'uniqid';
 
+type ToastMessage = {
+	type: 'info' | 'warning' | 'error' | 'success';
+	message: string;
+	id: string;
+};
+
 function createToastsStore() {
 	// store requires to return a subscribe()
-	const { subscribe } = writable([]);
+	const { subscribe } = writable<ToastMessage[]>([]);
 	return {
 		subscribe
 	};
