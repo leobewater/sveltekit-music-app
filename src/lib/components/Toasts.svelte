b1adf1fed0dcf5a2dd8e4aa9824ecc86cd5f030e
leobewater <mmbee888@gmail.com>
2024-04-01 09:13:22 -0400

    add toast css

--- src/lib/components/Toasts.svelte
+++ src/lib/components/Toasts.svelte
@@ -11,3 +11,31 @@
 		{/each}
 	</div>
 {/if}
+
+<style lang="scss">
+	.toasts {
+		position: fixed;
+		top: 0;
+		right: 0;
+		padding: 20px;
+		width: 100%;
+		max-width: 300px;
+		z-index: 9999;
+		max-height: 100vh;
+		overflow: auto;
+		.toast {
+			background-color: var(--accent-color);
+			box-shadow: 0 0 10px rgba(1, 1, 1, 0.2);
+			border-radius: 2px;
+			padding: 10px 20px;
+			margin-bottom: 10px;
+			font-weight: 500;
+			&.error {
+				background-color: var(--error);
+			}
+			&.warning {
+				background-color: var(--warning);
+			}
+		}
+	}
+</style>
