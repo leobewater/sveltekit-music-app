c8995067e8b43fc1d252e726b6e7fef8e172bd58
leobewater <mmbee888@gmail.com>
2024-04-01 10:50:53 -0400

    add form css

--- src/lib/components/PlaylistForm.svelte
+++ src/lib/components/PlaylistForm.svelte
@@ -4,13 +4,13 @@
 
 	export let form: AddActionData;
 	export let userID: string | undefined = undefined;
-  export let action: string | undefined = undefined;
+	export let action: string | undefined = undefined;
 </script>
 
 <form method="POST" {action}>
-  {#if userID}
-	  <input hidden name="userID" value={userID} />
-  {/if}
+	{#if userID}
+		<input hidden name="userID" value={userID} />
+	{/if}
 
 	<div class="field" class:has-error={form?.nameError}>
 		<label for="playlist-name">Name *</label>
@@ -42,3 +42,32 @@
 		<Button element="button" type="submit">Create Playlist</Button>
 	</div>
 </form>
+
+<style lang="scss">
+	form {
+		max-width: 400px;
+		.field {
+			margin-bottom: 20px;
+			&.has-error input {
+				outline: 2px solid var(--error);
+			}
+			label {
+				display: inline-block;
+				margin-bottom: 10px;
+				font-size: functions.toRem(14);
+			}
+			input {
+				width: 100%;
+			}
+		}
+		p.error {
+			color: var(--error);
+			font-size: functions.toRem(14);
+			margin: 10px 0 0;
+		}
+		.submit-button {
+			text-align: right;
+			margin-top: 40px;
+		}
+	}
+</style>
