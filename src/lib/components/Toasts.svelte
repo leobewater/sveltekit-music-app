fb6ee3fc8e1200cc6ebb2f8c72e81ce17c1c4290
leobewater <mmbee888@gmail.com>
2024-04-01 09:12:43 -0400

    add toasts to root layout

---
+++ src/lib/components/Toasts.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { toasts } from '$stores';
+</script>
+
+{#if $toasts.length > 0}
+	<div class="toasts">
+		{#each $toasts as toast (toast.id)}
+			<div class="toast {toast.type}">
+				<div class="content" role="status">{toast.message}</div>
+			</div>
+		{/each}
+	</div>
+{/if}
--- src/lib/stores/toasts.ts
+++ src/lib/stores/toasts.ts
@@ -9,7 +9,10 @@ type ToastMessage = {
 
 function createToastsStore() {
 	// store requires to return a subscribe()
-	const { subscribe } = writable<ToastMessage[]>([]);
+	const { subscribe } = writable<ToastMessage[]>([
+		{ type: 'info', message: 'some message', id: uniqid() },
+		{ type: 'error', message: 'some error message', id: uniqid() }
+	]);
 	return {
 		subscribe
 	};
